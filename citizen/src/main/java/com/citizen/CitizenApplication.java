61785e7fa52832fc8144ce779ed0389c6734d21b
AbdelheqMokhtari <hakoumokhtari6@gmail.com>
2025-03-14 11:19:41 +0100

    Improve code readability

--- citizen/src/main/java/com/citizen/CitizenApplication.java
+++ citizen/src/main/java/com/citizen/CitizenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CitizenApplication {
-
+	// The main method is the entry point of the application
 	public static void main(String[] args) {
 		SpringApplication.run(CitizenApplication.class, args);
 	}
